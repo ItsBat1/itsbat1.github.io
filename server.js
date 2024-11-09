9ebc34e3959d56c2b5f0715b7bd583dccf6abfc5
ItsBat1 <aleixgonzalez2010@gmail.com>
2024-11-09 21:14:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Instal·la les dependències: express i socket.io
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -10,22 +9,20 @@ const io = socketIo(server);
 // Serveix els fitxers HTML estàtics
 app.use(express.static(__dirname + '/public'));
 
-// Quan un usuari es connecta
 io.on('connection', (socket) => {
     console.log('Un usuari s\'ha connectat');
 
-    // Escolta els missatges enviats pels usuaris
+    // Escolta els missatges enviats pel client
     socket.on('chat message', (msg) => {
-        io.emit('chat message', msg); // envia el missatge a tots els usuaris
+        console.log(`Missatge rebut: ${msg}`); // Registra el missatge al servidor
+        io.emit('chat message', msg); // envia el missatge a tots els usuaris connectats
     });
 
-    // Quan un usuari es desconnecta
     socket.on('disconnect', () => {
         console.log('Un usuari s\'ha desconnectat');
     });
 });
 
-// Escolta en el port 3000 de la IP pública
 server.listen(3000, '0.0.0.0', () => {
     console.log('Servidor actiu a http://IP_DEL_SERVIDOR:3000');
 });
